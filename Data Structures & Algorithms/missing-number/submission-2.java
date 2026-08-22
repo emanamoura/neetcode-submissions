6708c548ae4a939e86680acffc106300694e6ac7
Emanuel Moura <94456436+emanamoura@users.noreply.github.com>
2026-08-22 08:38:14 -0300

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        int expectedSum = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+
+        for(int j = 0; j <= nums.length; j++) {
+            expectedSum += j;
+        }
+
+        System.out.println(sum);
+        System.out.println(expectedSum);
+
+        return expectedSum - sum;
+    }
+}
